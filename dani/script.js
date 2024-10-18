7c4f60303d01c956d9a1e3ee36c38510ae904b7a
daniliss19 <135145409+daniliss19@users.noreply.github.com>
2024-10-18 11:28:43 -0300

    Add files via upload

---
+++ dani/script.js
@@ -0,0 +1,33 @@
+documement.addEventListener('DOMContenTLoaded', function() { 
+    const items = document.querySelectorAll('.item');
+    const prevBtn = document.getElementByI('prevBtn');
+    const nextBtn = document.getElementByI('nextBtn');
+    const indicators = document.querySelectorAll('indicators li');
+    const numberDisplay = document.querySelector('.number');
+    let currentIndex = 0;
+
+    function showItem(index) {
+        items.forEach(item => item.classList.remove('active'));
+        indicators.forEach(indicator => indicator.classList.remove('active'))
+
+        items[index].classList.add('active');
+        indicators[index].classList.add('active');
+        numberDisplay.textContent = (index + 1).toString().padStart(2, '0');
+    }  
+    
+    function nextItem() {
+        currentIndex = (currentIndex + 1) % items.length;
+        showItem(currentIndex);
+    }
+
+    function prevItem() {
+        currentIndex = (currentIndex + 1) % items.length;
+        showItem(currentIndex);
+    }
+
+    nextBtn.addEventListener('click', nextItem);
+    prevBtn.addEventListener('click', prevItem);
+
+    // Inicializar
+    showItem(currentIndex);
+});
